d6b752dcac84db3c4ecfd4ad9178edfdc18edae6
dileep <dileep.thaniparthi@gmail.com>
2015-07-07 17:46:20 -0600

    Adding functionality for report links. Adding pages to view reports. Security issues exist when accessing a non local report server.

---
+++ SignetSSRProject2/SignetSSRProject/Controllers/ReportsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SignetSSRProject.Controllers
+{
+    public class ReportsController : Controller
+    {
+        //
+        // GET: /Reports/
+        public ActionResult Index()
+        {
+            //ViewData["Message"] = "Welcome to ssrs";
+            //return View();
+        //}
+        //public ActionResult ReportListing()
+        //{
+        return Redirect("../Reports/ReportList.aspx");
+
+    }
+            
+        }
+	}
---
+++ SignetSSRProject2/SignetSSRProject/Reports/Reportweb.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+//using System.Collections.Specialized.NameValueCollection;
+//using System.Web.UI.Page;
+//using System.Web.UI.ScriptManager;
+
+using Microsoft.Reporting.WebForms;
+
+namespace SignetSSRProject.Reports
+{
+    public partial class Reportweb : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        string Jobtype = HttpContext.Current.Request.QueryString["Jobtype"];
+        string Begindate =HttpContext.Current. Request.QueryString["Begindate"];
+        string Enddate = HttpContext.Current.Request.QueryString["Enddate"];
+   
+
+
+        /*protected void get_reports_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                myReportViewer.ProcessingMode = ProcessingMode.Remote;
+
+                myReportViewer.ServerReport.ReportServerUrl = new Uri("http://dileepthani9369:80/ReportServer");
+
+                myReportViewer.ServerReport.ReportPath = "/SSRS Project/Report1";
+
+                myReportViewer.ServerReport.Refresh();
+
+            }
+
+            catch (Exception ex)
+            {
+
+                Response.Write(ex.ToString());
+
+            }
+        }*/
+        
+    }
+}
